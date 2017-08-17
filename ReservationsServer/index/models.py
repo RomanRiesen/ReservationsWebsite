00ef4635809c481589e3fe70a9c16c25617fa194
RomanRiesen <roman.r97@gmx.ch>
2017-08-17 02:17:58 +0200

    initial Page added, but not 'connected.'

--- ReservationsServer/index/models.py
+++ ReservationsServer/index/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 # Create your models here.
+#FIXME TODO when PerformanceDate is changed, adjust the reservations!!!
+#FIXME TODO when PerformanceDate is deleted, delete the reservations!!!
 class PerformanceDate(models.Model):
     datum = models.DateField()
     has_free_seats = models.BooleanField(default = True)
@@ -13,3 +15,5 @@ class Reservation(models.Model):
     seatName = models.CharField(max_length = 255)
     email = models.CharField(max_length = 255)
     reservation_time = models.TimeField(auto_now_add = True)
+    #FIXME add action to change a users date of reservation
+    actions = []
